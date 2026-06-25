fa5cc7105b1a01fae8d50bab44281f9aeccde4b6
AgatsumaS <agatsuma746@gmail.com>
2026-06-26 02:55:19 +0900

    add-misuzu, add-terakoya途中

--- js/members-data.js
+++ js/members-data.js
@@ -1,25 +1,6 @@
 // カードの並び順はこの配列の順番です．certs は省略できます．
 window.memberData = [
   {
-    name: "塩見雄真",
-    kana: "しおみ ゆうま",
-    roman: "Shiomi Yuuma",
-    dept: "英米学科4年",
-    photo: "img/member/yuma.jpg",
-    alt: "塩見雄真のプロフィール写真",
-    message: "子ども達が集中して勉強し，かつ講師に対しても喜怒哀楽を出せる環境を創ります．",
-    certs: "英検準1級，教職免許証一種中・高英語(予定)"
-  },
-  {
-    name: "甲斐千尋",
-    kana: "かい ちひろ",
-    roman: "Kai Chihiro",
-    dept: "社会福祉学科3年",
-    photo: "img/member/chihiro.png",
-    alt: "甲斐千尋のプロフィール写真",
-    message: "ただ勉強ができる場所ではなく，安心できる居場所にもなれるよう意識しています．"
-  },
-  {
     name: "我妻 信実",
     kana: "あがつま しんじつ",
     roman: "Agatsuma Shinjitsu",
@@ -30,6 +11,16 @@ window.memberData = [
     certs: "英検5級，大型・大型特殊・牽引自動車第二種免許"
   },
   {
+    name: "塩見雄真",
+    kana: "しおみ ゆうま",
+    roman: "Shiomi Yuuma",
+    dept: "英米学科4年",
+    photo: "img/member/yuma.jpg",
+    alt: "塩見雄真のプロフィール写真",
+    message: "子ども達が集中して勉強し，かつ講師に対しても喜怒哀楽を出せる環境を創ります．",
+    certs: "英検準1級，教職免許証一種中・高英語(予定)"
+  },
+  {
     name: "堀 瑞葉",
     kana: "ほり みづは",
     roman: "Hori Mizuha",
@@ -37,5 +28,23 @@ window.memberData = [
     photo: "img/member/mizuha.jpeg",
     alt: "堀瑞葉のプロフィール写真",
     message: "あっぷすくーるに来たいって言ってもらえるような，楽しい・安心できる居場所をつくっていきます．"
+  },
+  {
+    name: "伊藤 美鈴",
+    kana: "いとう みすず",
+    roman: "Ito Misuzu",
+    dept: "国語国文学科3年",
+    photo: "img/member/misuzu.jpg",
+    alt: "伊藤美鈴のプロフィール写真",
+    message: "好きが高じて大学では日本文学を学んでいます．学ぶ楽しさを一緒に見つけていけるよう，丁寧にサポートしていきます．"
+  },
+  {
+    name: "甲斐千尋",
+    kana: "かい ちひろ",
+    roman: "Kai Chihiro",
+    dept: "社会福祉学科3年",
+    photo: "img/member/chihiro.png",
+    alt: "甲斐千尋のプロフィール写真",
+    message: "ただ勉強ができる場所ではなく，安心できる居場所にもなれるよう意識しています．"
   }
 ];
